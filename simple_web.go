c113ffbbf8981cd2b4d7617921fc88eaa323953c
hlwojiv <hlwojiv@gmail.com>
2019-12-19 11:33:38 +0800

    add upload

--- simple_web.go
+++ simple_web.go
@@ -1,29 +1,95 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "strings"
-    "log"
+	"crypto/md5"
+	"fmt"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
-    r.ParseForm()
-    fmt.Println(r.Form)
-    fmt.Println("path: ", r.URL.Path)
-    fmt.Println("scheme: ", r.URL.Scheme)
-    fmt.Println(r.Form["url_long"])
-    for k, v := range r.Form {
-        fmt.Println("key: ", k)
-        fmt.Println("val: ", strings.Join(v, ""))
-    }
-    fmt.Fprintf(w, "Hello astaxie!")
+	r.ParseForm()       //解析 url 传递的参数
+	fmt.Println(r.Form) // 输出至控制台
+	fmt.Println("path: ", r.URL.Path)
+	fmt.Println("scheme: ", r.URL.Scheme)
+	fmt.Println(r.Form["url_long"])
+	for k, v := range r.Form {
+		fmt.Println("key: ", k)
+		fmt.Println("val: ", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Hello hlwojiv!")
+}
+func login(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("method: ", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("login.gtpl")
+		t.Execute(w, token)
+	} else {
+		r.ParseForm()
+		token := r.Form.Get("token")
+		if token != "" {
+			// 验证 token 的合法性
+		} else {
+			// 不存在 token 报错
+		}
+		fmt.Println("username length:", len(r.Form["username"][0]))
+		fmt.Println("username:", template.HTMLEscapeString(r.Form.Get("username"))) //输出到服务器端
+		fmt.Println("password:", template.HTMLEscapeString(r.Form.Get("password")))
+		template.HTMLEscape(w, []byte(r.Form.Get("username"))) //输出到客户端
+
+		fmt.Println("username: ", r.Form["username"])
+		fmt.Println("password: ", r.Form["password"])
+	}
+}
+
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:　", r.Method)
+	switch r.Method {
+	case "GET":
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("upload.gtpl")
+		t.Execute(w, token)
+	case "POST":
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
 }
 
 func main() {
-    http.HandleFunc("/", sayhelloName)
-    err := http.ListenAndServe(":8082", nil)
-    if err != nil {
-        log.Fatal("ListenAndServe: ", err)
-    }
+	http.HandleFunc("/", sayhelloName)
+	http.HandleFunc("/login", login)
+	http.HandleFunc("/upload", upload)
+	err := http.ListenAndServe(":8082", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
